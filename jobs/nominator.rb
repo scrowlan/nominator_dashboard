788a1fa5d5f21964f4e31f7619714e93eaa17c2d
Scott Rowland <scrowlan@gmail.com>
2015-10-25 19:46:01 -0500

    updated scheduler

--- jobs/nominator.rb
+++ jobs/nominator.rb
@@ -1,24 +1,29 @@
 require 'net/http'
 require 'json'
 
-
-SCHEDULER.every '1m', :first_in => 0 do |job|
+SCHEDULER.every '1h', :first_in => 0 do |job|
   http = Net::HTTP.new "lightsaberaward.com"
   request = http.request Net::HTTP::Get.new("/nominations.json")
   website = JSON.parse request.body
-  random = website.sample
-  random_nomination = []
   
-  # random_nomination << { text: random['reason'], moreinfo: random['nominator'] }
-  reason = random['reason']
- # nominator = random['nominator']
-  fname = random['employee']['fname']
-  lname = random['employee']['lname']
-  if random['nominator'] != ""
-    nominator = random['nominator']
-  else
-    nominator = "anonymous"
+  SCHEDULER.every '2m', :first_in => 0 do |job|
+    random = website.sample
+    random_nomination = []
+    
+    # random_nomination << { text: random['reason'], moreinfo: random['nominator'] }
+    reason = random['reason']
+   # nominator = random['nominator']
+    fname = random['employee']['fname']
+    lname = random['employee']['lname']
+    if random['nominator'] != ""
+      nominator = random['nominator']
+    else
+      nominator = "anonymous"
+    end
+
+    send_event('nominator', { reason: reason, fname: fname, lname: lname, nominator: nominator} )
+
   end
 
-  send_event('nominator', { reason: reason, fname: fname, lname: lname, nominator: nominator} )
+
 end
